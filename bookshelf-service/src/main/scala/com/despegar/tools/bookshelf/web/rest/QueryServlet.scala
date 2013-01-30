b3f10b78e3a00619475d051f79ec91b31406af32
Facundo Viale <facundo.m.viale@gmail.com>
2013-01-29 21:48:46 -0300

    Add query service

---
+++ bookshelf-service/src/main/scala/com/despegar/tools/bookshelf/web/rest/QueryServlet.scala
@@ -0,0 +1,23 @@
+package com.despegar.tools.bookshelf.web.rest
+
+import com.despegar.tools.bookshelf.domain.dto.Project
+import com.despegar.tools.bookshelf.domain.dto.Enviroment
+
+class QueryServlet extends RestService {
+
+	
+	// This method can be improve A LOT in performace but i prefere a more declarative code
+	get("/:projectName/:moduleName/:enviromentName"){
+		
+		val enviroment = Enviroment.findByName( params("enviromentName") ).get
+		
+		Project.findByName(params("projectName")) match {
+			case Some(project) => project.modules.find( _.name equals params("moduleName") ) match {
+				case Some(module) => module.properties.map( _.value(enviroment) ) 
+				case None => 
+			}
+			case None => 
+		}
+	}
+	
+}
